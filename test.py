d271c539d3c9266fc632db961d45a6eadf05e721
Jakob Schechter <jakeeli52@gmail.com>
2021-10-12 14:34:55 -0400

    Updated test.py - Added line segment generation to the test script - Added box collider drawing to the test script

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import svgwrite as svg
 import aabbtree as aabb
 
 bounds = [0, 0, 2000, 2000]
-area = BoxCollider(rect = [bounds[:2:], bounds[2::]])
+area = BoxCollider(rect = np.array([bounds[:2:], bounds[2::]]))
 tree = aabb.AABBTree()
 
 initial_option = BranchOption(
@@ -18,31 +18,41 @@ initial_option = BranchOption(
 
 options = [initial_option]
 
+def gen_circle(option):
+    return Circle(branch = option, padding = 9, radius = random.uniform(10, 80))
+
+def gen_line(option):
+    return LineSegment(branch = option, length = random.uniform(50, 200), padding = 9)
+
 count = 0
 while count < 300:
     index = random.randint(0, len(options) - 1)
     options[index], options[-1] = options[-1], options[index] # swap a random index to the end
     option = options.pop()
 
-    element = Circle(
-        branch = option,
-        padding = 5,
-        radius = random.uniform(10, 80),
-    )
+    element = random.choice([
+        gen_circle,
+        gen_line
+    ])(option)
     
     if element.box.overlaps_with(area) and len(element.box.get_overlapping(tree)) <= 1:
         tree.add(element.box.box, element)
         new_options = element.get_branch_options()
         options.append(new_options[0])
-        options.append(new_options[1][0])
-        options.append(new_options[1][1])
+        if isinstance(element, Circle):
+            options.append(new_options[1][0])
+            options.append(new_options[1][1])
         count += 1
     elif len(options) <= 1:
         options.append(option)
 
 dwg = svg.Drawing("test.svg", ('20cm', '20cm'))
 dwg.viewbox(*bounds)
-for element in area.get_overlapping(tree):
+
+all_elements = area.get_overlapping(tree)
+for element in all_elements:
     element.draw(dwg, line_properties = {'stroke_width': 9, 'stroke': svg.rgb(0,0,0,'rgb'),})
+for element in all_elements:
+    element.box.draw_box(dwg, line_properties = {'stroke_width': 3, 'stroke': svg.rgb(255,0,0,'rgb'),})
 
 dwg.save()
