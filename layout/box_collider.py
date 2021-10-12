e3ff6cbae22ae7fdb367b50809d3ec7456db2415
Jakob Schechter <jakeeli52@gmail.com>
2021-10-12 14:33:40 -0400

    Added draw_box method to box_collider.py

--- layout/box_collider.py
+++ layout/box_collider.py
@@ -2,7 +2,7 @@ from aabbtree import *
 
 class BoxCollider():
     def __init__(self, *args, **kwargs):
-        rect = kwargs.pop('rect') # rect must be of form [[x1, y1], [x2, y2]...]
+        rect = [point.tolist() for point in kwargs.pop('rect')] # rect must be of form [[x1, y1], [x2, y2]...]
         rect = zip(*rect) # rearrange rect to [[x1, x2...], [y1, y2...]]
         rect = tuple((min(pair), max(pair)) for pair in rect) # rearrange rect to [[minX, maxX], [minY, maxY]]
         self.box = AABB(rect)
@@ -12,3 +12,8 @@ class BoxCollider():
 
     def get_overlapping(self, box_tree):
         return box_tree.overlap_values(self.box)
+
+    def draw_box(self, drawing, line_properties):
+        rect_data = [(p[0], p[1] - p[0] + 1) for p in self.box.limits]
+        rect_data = tuple(zip(*rect_data))
+        drawing.add(drawing.rect(*rect_data, fill='none', **line_properties))
