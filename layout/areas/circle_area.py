d9579a33040145f9d3ef3275a5e9121b8b6bb37e
Jakob Schechter <jakeeli52@gmail.com>
2021-10-27 12:22:05 -0400

    Changed iterator name in circle_area.py

--- layout/areas/circle_area.py
+++ layout/areas/circle_area.py
@@ -6,7 +6,7 @@ from . import CompoundArea, RectangularArea
 
 def get_circle_area(center, radius, resolution):
     rects = range(1,resolution)
-    rects = ((angle * pi) / (2 * resolution) for angle in rects)
+    rects = ((step * pi) / (2 * resolution) for step in rects)
     rects = (np.array([cos(angle), sin(angle)]) for angle in rects)
     rects = ([center + point * radius, center - point * radius]
               for point in rects)
